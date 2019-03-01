bdf6554005732f940406cd0b0b0d79c26f0f0b02
阿振_sc <3494688@qq.com>
2019-03-01 14:37:07 +0800

    修改测试环境配置

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -2,8 +2,8 @@
  * @Description: axios网络请求库二次封装
  * @Author: LiuZhen
  * @Date: 2018-09-18 11:49:38
- * @Last Modified by: chenmo
- * @Last Modified time: 2019-02-20 18:02:17
+ * @Last Modified by: LiuZhen
+ * @Last Modified time: 2019-03-01 14:36:41
  */
 import axios from 'axios';
 import store from '../store';
@@ -20,10 +20,10 @@ const Axios = axios.create({
 Axios.interceptors.request.use(
   (config: any) => {
     // 根据环境设置baseURL
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'production' && !process.env.VUE_APP_TEST) {
       config.baseURL = 'http://api-gateway.uoko.com/';
       // config.baseURL = 'http://bi.uoko.com:9999/app/webapi/';
-    } else if (process.env.NODE_ENV === 'test') {
+    } else if (process.env.NODE_ENV === 'production' && process.env.VUE_APP_TEST) {
       config.baseURL = 'http://192.168.200.120:7070/';
     } else {
       // config.baseURL = 'http://192.168.200.120:7070/'; // 测试环境
