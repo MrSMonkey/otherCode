c6c153be53711c0c096f093b1ebecfdf1b80fbb9
chenmo <chenmo@uoko.com>
2019-03-26 21:02:48 +0800

    增加继续支付

--- src/views/ProductDetile.vue
+++ src/views/ProductDetile.vue
@@ -9,7 +9,7 @@
 <template>
   <section class="order-detils">
     <div class="order-info">
-      <div><h2>服务产品订单信息</h2></div>
+      <div class="order-title"><h2>服务产品订单信息</h2><span>{{orderInfo.orderStatusName}}</span></div>
       <div class='serviceName'>
         <span>服务产品名称：{{orderInfo.productName || '无'}}</span>
       </div>
@@ -27,7 +27,7 @@
       </div>
     </div>
     <div class="order-info">
-      <h2>其他信息</h2>
+      <div class="order-title"><h2>其他信息</h2></div>
       <div class='serviceName'>
         <span>服务房源：{{orderInfo.productHouseName || '无'}}</span>
       </div>
@@ -42,7 +42,8 @@
       </div>
     </div>
     <section>
-      <van-button slot="button" size="large" type="default" class="entrust-btn" @click="pushRecord">查看服务历史</van-button>
+      <van-button slot="button" size="large" type="default" class="entrust-btn" @click="paymentData" v-if ="orderInfo.orderType!==4 && orderInfo.orderStatus === 1">继续支付</van-button>
+      <van-button slot="button" size="large" type="default" class="entrust-btn" @click="pushRecord" v-else>查看服务历史</van-button>
     </section>
   </section>
 </template>
@@ -51,10 +52,11 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { State, Getter, Mutation, Action } from 'vuex-class';
 import CommonMixins from '@/utils/mixins/commonMixins';
-import { getQueryString } from '@/utils/utils';
-
+import { getQueryString, returnDomain } from '@/utils/utils';
 import { STATUS_NAME } from '@/config/config';
 import api from '@/api';
+const namespace: string = 'global';
+
 
 // 声明引入的组件
 @Component({
@@ -66,9 +68,11 @@ export default class ProductDetile extends CommonMixins {
   private entrustId: string = ''; // 房源id
   private orderInfo: any = {}; // 服务订单详情
 
+  @Action('payment', { namespace }) private payment: any;
+
   private mounted() {
-    this.orderId = String(this.$route.query.orderId);
-    this.entrustId =  String(this.$route.query.entrustId);
+    this.orderId = String(this.$route.query.orderId).split('?')[0];
+    this.entrustId =  String(this.$route.query.entrustId).split('?')[0];
     this.getServiceOrder(this.orderId); // 获取订单列表
   }
 
@@ -133,21 +137,43 @@ export default class ProductDetile extends CommonMixins {
       this.$router.push(`/serviceRecord?orderId=${this.orderId}&entrustId=${this.entrustId}`);
     }
   }
+
+  /**
+   * @description 继续支付
+   * @returns void
+   * @author chenmo
+   */
+  private paymentData() {
+    const status: string = this.orderInfo.orderType === 9 ? '2' : '1'; // 装修不需要发起服务
+    const data  = {
+      orderId: this.orderInfo.orderId,
+      returnURL: `${returnDomain()}productDetile?entrustId=${this.entrustId}&orderId=${this.orderInfo.orderId}&status=${status}`,
+    };
+    this.payment(data); // 发起支付
+  }
 }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus" scoped>
 @import '../assets/stylus/main.styl'
 .order-detils
   .order-info
     padding 0 vw(15) vw(12)
     background $global-background
     margin-bottom vw(10)
+  .order-title
+    display -webkit-flex
+    display flex
+    justify-content space-between
+    align-items center
+    border-bottom 1px solid $border-color-light
+    span
+     color $main-color
+     font-size 15px
   h2
     padding vw(12) 0
     font-size 16px
     color $text-color
-    border-bottom 1px solid $border-color-light
   .serviceName
     font-size 15px
     color $text-color
@@ -194,4 +220,5 @@ export default class ProductDetile extends CommonMixins {
   left 0
   background $main-color
   color #fff
+  z-index 10
 </style>
--- src/views/ServiceDetile.vue
+++ src/views/ServiceDetile.vue
@@ -8,51 +8,56 @@
 
 <template>
   <section class="order-detils">
-    <div class="order-info">
-      <h2>服务包订单信息</h2>
-      <div class='serviceName'>
-        <span>服务包名称：{{orderInfo.servicePackage || '无'}}</span>
-      </div>
-      <div class='serviceName'>
-        <span>服务包订单号：{{orderInfo.servicePackageOrderNo || '无'}}</span>
-      </div>
-      <div class='serviceName'>
-        <span>下单时间：{{orderInfo.createTime|| '无'}}</span>
-      </div>
-      <div class='serviceName'>
-        <span>实付款：{{orderInfo.price || 0 }}元</span>
-      </div>
-    </div>
-    <div class="order-info">
-      <h2>服务包订单信息</h2>
-      <div class='order-type' v-for="(item, index) in orderInfo.serviceItems" :key="index">
-        <div class="order-item">
-          <span>订单号：{{item.orderNo || '无'}}</span>
-          <div :class="item.status === 7 ? '' : 'order-item-type'">{{getOrderStatusName(item.status)}}</div>
+    <section class="order">
+      <div class="order-info">
+        <h2>服务包订单信息</h2>
+        <div class='serviceName'>
+          <span>服务包名称：{{orderInfo.servicePackage || '无'}}</span>
         </div>
-        <div class="order-item">
-          <span>产品类型：{{item.serviceType || '无'}}</span><span class="check" @click="checkout(item.serviceId, item.orderType)">查看服务记录<van-icon name="arrow" /></span>
+        <div class='serviceName'>
+          <span>服务包订单号：{{orderInfo.servicePackageOrderNo || '无'}}</span>
         </div>
-        <div class="order-item">
-          <span>产品名称：{{item.serviceName || '无'}}</span>
+        <div class='serviceName'>
+          <span>下单时间：{{orderInfo.createTime|| '无'}}</span>
+        </div>
+        <div class='serviceName'>
+          <span>实付款：{{orderInfo.price || 0 }}元</span>
         </div>
       </div>
-    </div>
-    <div class="order-info">
-      <h2>其他信息</h2>
-      <div class='serviceName'>
-        <span>服务房源：{{orderInfo.serviceHouseName || '无'}}</span>
+      <div class="order-info">
+        <h2>服务包订单信息</h2>
+        <div class='order-type' v-for="(item, index) in orderInfo.serviceItems" :key="index">
+          <div class="order-item">
+            <span>订单号：{{item.orderNo || '无'}}</span>
+            <div :class="item.status === 7 ? '' : 'order-item-type'">{{getOrderStatusName(item.status)}}</div>
+          </div>
+          <div class="order-item">
+            <span>产品类型：{{item.serviceType || '无'}}</span><span class="check" @click="checkout(item.serviceId, item.orderType)" v-if="orderInfo.orderStatus !==1">查看服务记录<van-icon name="arrow" /></span>
+          </div>
+          <div class="order-item">
+            <span>产品名称：{{item.serviceName || '无'}}</span>
+          </div>
+        </div>
       </div>
-      <div class='serviceName'>
-        <span>联系人：{{orderInfo.houseContact || '无'}}</span>
+      <div class="order-info">
+        <h2>其他信息</h2>
+        <div class='serviceName'>
+          <span>服务房源：{{orderInfo.serviceHouseName || '无'}}</span>
+        </div>
+        <div class='serviceName'>
+          <span>联系人：{{orderInfo.houseContact || '无'}}</span>
+        </div>
+        <div class='serviceName'>
+          <span>联系电话：{{orderInfo.houseContactPhone|| '无'}}</span>
+        </div>
+        <div class='serviceName'>
+          <span>备      注：{{orderInfo.remark }}</span>
+        </div>
       </div>
-      <div class='serviceName'>
-        <span>联系电话：{{orderInfo.houseContactPhone|| '无'}}</span>
-      </div>
-      <div class='serviceName'>
-        <span>备      注：{{orderInfo.remark }}</span>
-      </div>
-    </div>
+    </section>
+    <section>
+      <van-button slot="button" size="large" type="default" class="entrust-btn" @click="paymentData" v-if ="orderInfo.orderStatus === 1">继续支付</van-button>
+    </section>
   </section>
 </template>
 
@@ -60,10 +65,10 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { State, Getter, Mutation, Action } from 'vuex-class';
 import CommonMixins from '@/utils/mixins/commonMixins';
-import { getQueryString } from '@/utils/utils';
-
+import { getQueryString, returnDomain } from '@/utils/utils';
 import { STATUS_NAME } from '@/config/config';
 import api from '@/api';
+const namespace: string = 'global';
 
 // 声明引入的组件
 @Component({
@@ -72,10 +77,14 @@ import api from '@/api';
 // 类方式声明当前组件
 export default class ServiceDetile extends CommonMixins {
   private servicePackageId: string = ''; // 订单id
+  private entrustId: string = ''; // 房源id
   private orderInfo: any = {}; // 服务订单详情
+
+  @Action('payment', { namespace }) private payment: any;
 
   private mounted() {
     this.servicePackageId = String(this.$route.query.orderId).split('?')[0];
+    this.entrustId =  String(this.$route.query.entrustId).split('?')[0];
     this.getServiceOrder(this.servicePackageId); // 获取订单列表
   }
 
@@ -130,12 +139,28 @@ export default class ServiceDetile extends CommonMixins {
       this.$router.push(`/serviceRecord?orderId=${orderId}`);
     }
   }
+
+  /**
+   * @description 继续支付
+   * @returns void
+   * @author chenmo
+   */
+  private paymentData() {
+    const data  = {
+      orderId: this.orderInfo.servicePackageId,
+      returnURL: `${returnDomain()}serviceDetile?entrustId=${this.entrustId}&orderId=${this.orderInfo.servicePackageId}`
+    };
+    this.payment(data); // 发起支付
+  }
 }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus" scoped>
 @import '../assets/stylus/main.styl'
 .order-detils
+  height auto
+  .order
+    margin-bottom vw(80)
   .order-info
     padding 0 vw(15) vw(12)
     background $global-background
@@ -176,6 +201,12 @@ export default class ServiceDetile extends CommonMixins {
           margin-top vw(1)
           font-size 15px
           vertical-align top
+  .entrust-btn
+    position fixed !important
+    bottom 0
+    left 0
+    background $main-color
+    color #fff
 .dialogTips
   width 70%
   .van-dialog__header
@@ -185,4 +216,5 @@ export default class ServiceDetile extends CommonMixins {
   .van-dialog__message--has-title
     color #2C2D2E
     font-size 14px
+
 </style>
