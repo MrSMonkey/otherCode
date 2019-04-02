7c58a2d03f9c8efa18ad333e9dce4b3e89eeaa20
chenmo <chenmo@uoko.com>
2019-04-02 16:46:02 +0800

    修改项目目录名称

--- src/views/ServiceRecord/ServiceRecord.vue
+++ src/views/ServiceRecord/ServiceRecord.vue
@@ -59,7 +59,7 @@ import { Action } from 'vuex-class';
 import CommonMixins from '@/utils/mixins/commonMixins';
 import { handleWebStorage, solveScrollBug,  returnDomain } from '@/utils/utils';
 import { Button, CellGroup, Field, Icon} from 'vant';
-import HouseStatus from '../HouseDetile/components/HouseStatus.vue';
+import HouseStatus from '../HouseDetail/components/HouseStatus.vue';
 import { STATUS_NAME } from '@/config/config';
 import api from '@/api';
 const namespace: string = 'global';
