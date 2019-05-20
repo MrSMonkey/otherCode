07f8325b5275fd4dc5a0f770a5a62033e98a4317
chenmo <chenmo@uoko.com>
2019-05-20 11:02:09 +0800

    修改baseUrl

--- src/config/baseURLConfig.ts
+++ src/config/baseURLConfig.ts
@@ -2,8 +2,8 @@
  * @Description: linyu
  * @Author: linyu
  * @Date: 2019-05-15 16:07:32
- * @Last Modified by: linyu
- * @Last Modified time: 2019-05-17 16:23:33
+ * @Last Modified by: chenmo
+ * @Last Modified time: 2019-05-20 10:54:17
  */
 
 // 根据环境设置应用内部请求的baseURL
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production' && process.env.VUE_APP_TITLE === 'prod
 } else {
   // appBaseURL = 'http://front-end.testuoko.com:3000/mock/22/'; // mock地址
   // appBaseURL = 'http://192.168.200.44:7070/';
-  appBaseURL = 'https://api-gateway-pre.uoko.com/';
+  appBaseURL = 'http://api-gateway.testuoko.com/';
   // appBaseURL = 'http://api-gateway.testuoko.com/';
   // appBaseURL = 'http://172.16.3.3:7070/';
   // appBaseURL = 'http://172.16.3.103:8008';
